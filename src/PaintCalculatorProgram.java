4596d2ca8f46b530db3f3613228a4456225a556c
davidbosnjak <davidbosnjak2@gmail.com>
2022-10-01 18:23:09 -0400

    big changes

--- src/PaintCalculatorProgram.java
+++ src/PaintCalculatorProgram.java
@@ -1,58 +1,80 @@
-import javax.swing.*;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Scanner;
-import java.text.DecimalFormat;
+
 public class PaintCalculatorProgram {
 
     public static void main(String[] args) {
-        int SQUARE_CM_IN_SQUARE_M = 10000;
-        //test
-        int METERS_PER_PAINT = 11;
-        double PAINT_IN_CAN = 3.78f;
-        int SEMIGLOSS_PER_WALL = 2;
-        int PRIMER_PER_WALL = 1;
-        Scanner scanner = new Scanner(System.in);
+        boolean continueProgram = true;
+        while(continueProgram){
+            paintCalculator();
+            System.out.println("Do you want to make another calculation? y/n");
+            char userSelection = new Scanner(System.in).next().charAt(0);
+            if(userSelection== 'y' || userSelection == 'Y'){ continueProgram = true;}
+            else{continueProgram = false;}
+        }
+    }
+    public static void paintCalculator(){
+        //Declaring constants
+        final double SQUARE_CM_IN_SQUARE_M = 10000;
+        final int METERS_PER_PAINT = 11;
+        final double PAINT_IN_CAN = 3.78f;
+        final int SEMIGLOSS_PER_WALL = 2;
+        final int PRIMER_PER_WALL = 1;
+
+        //Getting user inputs for dimensions of the room
         System.out.println("Please enter length of the room");
-        int roomLength = scanner.nextInt();
+        int roomLength = checkInput();
         System.out.println("Please enter width of the room");
-        int roomWidth = scanner.nextInt();
+        int roomWidth = checkInput();
         System.out.println("Please enter height of the room");
-        int roomHeight = scanner.nextInt();
+        int roomHeight = checkInput();
+
+        //Get the surface area on the walls based on the input
         int answerCM = getWallArea(roomLength, roomWidth, roomHeight);
-        System.out.println(answerCM);
+
+        //use conversion factors to get all the desired values
         double answerM = answerCM/SQUARE_CM_IN_SQUARE_M;
-        System.out.println("Surface area: "+answerM);
         double paintLitresRequired = answerM / METERS_PER_PAINT;
-        System.out.println("You will need "+paintLitresRequired);
         double paintCansRequiredDecimal = paintLitresRequired / PAINT_IN_CAN;
         double primerCansRequiredDecimal = paintCansRequiredDecimal * PRIMER_PER_WALL;
         double semiCansRequiredDecimal = paintCansRequiredDecimal * SEMIGLOSS_PER_WALL;
 
+        //finding the ceiling of the cans required because you cannot buy a fraction of a can of paint, you will need a full one
         int semiCansRequiredInt = (int) Math.ceil(semiCansRequiredDecimal);
-
-
         int primerCansRequiredInt  = (int) Math.ceil(primerCansRequiredDecimal);
 
-        System.out.println("You will need "+primerCansRequiredInt+" cans of primer");
-        System.out.println("Your calculations for:");
-        System.out.println("Length: "+roomLength+"\nWidth: "+roomWidth+"\nHeight: "+roomHeight+"\n");
-        System.out.println("Total surface area to be painted: "+answerCM+"cm^2, "+answerM+"m^2");
-        System.out.printf("Litres of paint required per coat: %.2f \n",paintLitresRequired);
-        System.out.println("Cans of primer needed: "+primerCansRequiredInt+"\nCans of semi-gloss needed: "+semiCansRequiredInt);
+        //rounding to 3 significant digits
+        paintLitresRequired = roundDouble(paintLitresRequired, 3);
 
+        //breaking up the output into different strings
+        String sSurfaceArea = "Total surface area to be painted: "+ answerM+"m\u00B2";
+        String sLitres = "Litres of paint required per coat: "+paintLitresRequired+"L";
+        String sIntro = "Your calculations for: \nlength: "+roomLength+" width: "+roomWidth+" height: "+roomHeight+"";
+        String sCans = "Cans of primed required: "+primerCansRequiredInt+"\nCans of semi-gloss required: "+semiCansRequiredInt;
 
+        //if the area is a whole number, remove the redundant decimal at the end
+        if(answerM == Math.floor(answerM)){
+            int roundedSurfaceArea = (int)Math.floor(answerM);
+            sSurfaceArea = "Total surface area to be painted: "+ roundedSurfaceArea+"m"+"\u00B2";
+        }
+        //If the volume is a whole number remove the decimal at the end
+        if(paintLitresRequired == Math.floor(paintLitresRequired)){
+            int intPaintLitresRequired = (int)Math.floor(paintLitresRequired);
+            sLitres = "Litres of paint required per coat: "+intPaintLitresRequired+"L";
 
-
+        }
+        //print out all the strings in order
+        System.out.println(sIntro);
+        System.out.println(sSurfaceArea);
+        System.out.println(sLitres);
+        System.out.println(sCans);
     }
-    public static short checkJOptionInput(String message){
+    public static short checkInput(){
         short parsedNumber;
 
         //infinite loop until an appropriate value is inputted by the user
         while(true){
-
+            //setup scanner object to read next line from user
             Scanner scanner = new Scanner(System.in);
-            System.out.println(message);
             String userInput =scanner.nextLine();
 
             //if a short can successfully be parsed from the input string without error, it will break out of the loop. Otherwise, it will keep asking for a correct input and showing an error message
@@ -65,6 +87,7 @@ public class PaintCalculatorProgram {
                     break;
                 }
             }
+            //If the inputted value is not a number let the user know
             catch(NumberFormatException e){
                 System.out.println("Error! "+userInput+" is not a valid number!");
             }
@@ -72,18 +95,32 @@ public class PaintCalculatorProgram {
         return parsedNumber;
     }
     public static double roundDouble(double numToRound, int digitsToShow){
+        //This condition exists because if 0 is picked, there will be an infinite loop as for {XeR}, 0*X = 0
+        if(numToRound == 0) return 0;
 
-        //using the BigDecimal class to round a double to a variable degree of precision and setting the rounding mode to HALF_UP which is the standard system taught in schools
-        BigDecimal bgNumToRound = new BigDecimal(numToRound).setScale(digitsToShow, RoundingMode.HALF_UP);
+        //This method works by multiplying a given decimal by 10 until it's an x digit number, once it's an x digit number we floor it, and we're left with only x digits.
+        //Then we divide it by the same number we multiplied it by, and we're left with a decimal of x significant digits
+        final int FACTOR = 10;
+        int count = 1;
 
-        //converting the BigDecimal object to a double and returning it
-        return bgNumToRound.doubleValue();
+        int intNumToRound;
+        while(numToRound<Math.pow(10,digitsToShow-1)){
+            numToRound *=FACTOR;
+            count++;
+        }
+
+        intNumToRound= (int)Math.floor(numToRound);
+        numToRound = intNumToRound;
+
+
+        numToRound/=(Math.pow(FACTOR,count-1));
+
+
+        return numToRound;
     }
+    //method that calculates the surface area of the walls based on the dimensions
     public static int getWallArea(int length, int width, int height){
-        int returnValue = length*height*2+ width*height*2;
-        return returnValue;
-
-
+        return length*height*2+ width*height*2;
 
     }
 }
--- src/PaintCalculatorProgram2.java
+++ src/PaintCalculatorProgram2.java
@@ -1,113 +1,119 @@
-
+//Imports
+//Everything from javax.swing because I'm using lots of classes from it
 import javax.swing.*;
-import java.awt.*;
+//Font class for making the title of the window look better
+import java.awt.Font;
+
+//classes for events and event listening. For example, button presses are an event I'm listening for
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-
 public class PaintCalculatorProgram2 implements ActionListener {
+    //Declaring all objects that must be accessed from multiple class methods. Buttons and menus must be accessed both from the main method and the event action method
     public static JButton submitButton = new JButton("Start Calculation");
     static String[] unitChoices = {"Meters","Centimeters","Feet","Inches"};
     static Integer[] paintChoices = {1,2,3,4,5};
     static Integer[] precisionChoices = {1,2,3,4,5,6};
     static String[] roundingChoices = {"Integer", "Double"};
     static JComboBox<String> unitMenu = new JComboBox<>(unitChoices);
-
     static String[] paintVolumeOptions = {"Default(3.78)","Custom"};
     static JComboBox<String> paintVolumeSelector = new JComboBox<>(paintVolumeOptions);
-
     static JComboBox<Integer> primerSelector = new JComboBox<>(paintChoices);
     static JComboBox<Integer> semiSelector = new JComboBox<>(paintChoices);
-
     static JComboBox<Integer> precisionSelector = new JComboBox<>(precisionChoices);
-
     static JComboBox<String> roundingModeSelector = new JComboBox<>(roundingChoices);
     static final double defaultPaintVolume = 3.78;
-
     static JPanel panel = new JPanel();
 
-
-
+    //main method
     public static void main(String[] args){
 
+        //Creating a new window using JFrame, setting close behavior, setting a title and size
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Paint Calculator Pro");
         frame.setSize(400,400);
 
+        //Adding a panel to the frame. I will put objects on the panel
         frame.add(panel);
 
+        //Not using any layouts like gridlayouts. In this program I manually set the size and position of each object
         panel.setLayout(null);
 
+        //Main label of the window in a bigger and bolder font
         JLabel titleMessage = new JLabel("Paint Calculator Pro");
         titleMessage.setFont(new Font("Times", Font.BOLD,20));
         titleMessage.setBounds(50,20,400,25);
         panel.add(titleMessage);
 
+        //Label that indicates that the dropdown menu is for unit selection
         JLabel unitMessage = new JLabel("Select Unit:");
         unitMessage.setBounds(30,60,150,20);
         panel.add(unitMessage);
 
-
-
+        //Adding a dropdown menu for selecting the desired unit of measurement
         unitMenu.addActionListener(new PaintCalculatorProgram2());
         unitMenu.setBounds(30,80,120,25);
         panel.add(unitMenu);
 
+        //Adding a button that submits the information on the page
         submitButton.addActionListener(new PaintCalculatorProgram2());
         submitButton.setBounds(200,300,150,30);
         panel.add(submitButton);
         frame.setVisible(true);
 
+        //Adding can volume label
         JLabel paintVolumeMessage = new JLabel("Select can volume:");
         paintVolumeMessage.setBounds(30,110,150,20);
         panel.add(paintVolumeMessage);
 
+        //Adding paint can volume dropdown menu. Also adding an a call to the action listener here because the program will need to know when the user has selected a custom paint can volume
         paintVolumeSelector.setBounds(30,130,120,20);
         paintVolumeSelector.addActionListener(new PaintCalculatorProgram2());
         panel.add(paintVolumeSelector);
 
+        //Adding primer coats label
         JLabel primerCoats = new JLabel("Select # of primer coats");
         primerCoats.setBounds(30,160,150,20);
         panel.add(primerCoats);
 
+        //Adding the number of primer coats selection dropdown menu
         primerSelector.setBounds(30,180,120,20);
-        primerSelector.addActionListener(new PaintCalculatorProgram2());
-
-
         panel.add(primerSelector);
 
+        //Adding a semi coats label
         JLabel semiCoats = new JLabel("Select # of semi coats");
         semiCoats.setBounds(30,210,150,20);
         panel.add(semiCoats);
 
+        //Adding the number of semi coats selection dropdown menu
         semiSelector.setBounds(30,230,120,20);
-        semiSelector.addActionListener(new PaintCalculatorProgram2());
         panel.add(semiSelector);
 
-
+        //Adding the precision setting label
         JLabel precisionSetLabel = new JLabel("Sig Digs paint volume");
         precisionSetLabel.setBounds(200,60,200,20);
         panel.add(precisionSetLabel);
+
+        //Adding the precision selection dropdown menu. Select the number of significant digits you want to see in the paint volume.
         precisionSelector.setBounds(200,80,120,20);
-        precisionSelector.addActionListener(new PaintCalculatorProgram2());
         panel.add(precisionSelector);
 
+        //Adding the rounding mode label
         JLabel areaRoundingLabel = new JLabel("Area rounding mode");
         areaRoundingLabel.setBounds(200,110,180,20);
         panel.add(areaRoundingLabel);
 
+        //Adding the rounding mode selection menu for the area you'll see in the unit of your choice. Options are either double or integer.
         roundingModeSelector.setBounds(200,130, 120,20);
-        roundingModeSelector.addActionListener(new PaintCalculatorProgram2());
         panel.add(roundingModeSelector);
 
 
-
+        //Setting the default values for the dropdown menus so Mr. Lauder doesn't have to mess with the dropdowns to test the table data.
         semiSelector.setSelectedItem(2);
         precisionSelector.setSelectedItem(3);
-
-
+        roundingModeSelector.setSelectedItem(roundingChoices[1]);
 
     }
     //method to get the surface area of the walls based on dimensions of the room
@@ -121,55 +127,61 @@ public class PaintCalculatorProgram2 implements ActionListener {
         //infinite loop until an appropriate value is inputted by the user
         while(true){
 
-
+            //Showing a JOptionInput screen with the desired message
             String userInput = JOptionPane.showInputDialog(null, message);
+
+            //If the user clicked on cancel or the x button the default value gets entered
             if(userInput == null){
                 parsedNumber = (short)defaultValue;
                 break;
             }
             //if a short can successfully be parsed from the input string without error, it will break out of the loop. Otherwise, it will keep asking for a correct input and showing an error message
             try{
+                //this function has 2 modes, one for strictly whole numbers and one for doubles
                 if(mode ==0) {
                     parsedNumber = Short.parseShort(userInput);
                 }
                 else{
                    parsedNumber = Double.parseDouble(userInput);
                 }
+                //checking for negative numbers since having them would make no sense in any context
                 if(parsedNumber <0){
                     JOptionPane.showMessageDialog(null, "No negative numbers! You can't have a room with negative side lengths!","Error!", JOptionPane.ERROR_MESSAGE);
                 }
+                //if everything goes well, break out of the loop
                 else {
                     break;
                 }
-            }catch(NumberFormatException e){
+            }
+            //If the user did not input the desired type show an error message
+            catch(NumberFormatException e){
                 JOptionPane.showMessageDialog(null, "Enter a valid number! \n"+userInput+" is not a valid number!","Error!", JOptionPane.ERROR_MESSAGE);
             }
+            //If anything else weird happened show an error message
             catch(Exception e){
-                JOptionPane.showMessageDialog(null, "Exception occured!","Error!", JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(null, "Exception occurred!","Error!", JOptionPane.ERROR_MESSAGE);
             }
         }
         return parsedNumber;
     }
     //method to round a double to a certain degree of precision
     public static double roundDouble(double numToRound, int digitsToShow){
+        //This condition exists because if 0 is picked, there will be an infinite loop as for {XeR}, 0*X = 0
         if(numToRound == 0) return 0;
-        //using the BigDecimal class to round a double to a variable degree of precision and setting the rounding mode to HALF_UP which is the standard system taught in schools
+
+        //This method works by multiplying a given decimal by 10 until it's an x digit number, once it's an x digit number we floor it, and we're left with only x digits.
+        //Then we divide it by the same number we multiplied it by, and we're left with a decimal of x significant digits
         final int FACTOR = 10;
         int count = 1;
-        //creating another variable may seem redundant, but it helps fix a weird but where it doesn't round properly if you pick very small values for dimensions of the room
+
         int intNumToRound;
         while(numToRound<Math.pow(10,digitsToShow-1)){
             numToRound *=FACTOR;
             count++;
         }
-        System.out.println(numToRound);
         intNumToRound= (int)Math.floor(numToRound);
         numToRound = intNumToRound;
-        System.out.println(numToRound);
-
         numToRound/=(Math.pow(FACTOR,count-1));
-
-
         return numToRound;
     }
     public static void openPaintProgram(int unit, double canVolume, int primerPerWall, int semiPerWall, int sigDigs, int roundingMode) {
@@ -177,14 +189,14 @@ public class PaintCalculatorProgram2 implements ActionListener {
         final short SQUARE_CM_IN_SQUARE_M = 10000;
         final double SQUARE_FEET_IN_SQUARE_M = 10.7639;
         final short SQUARE_INCH_IN_SQUARE_M = 1550;
-
         final short METERS_PER_PAINT = 11;
+
+        //Constants chosen by the user
         final double PAINT_IN_CAN = canVolume;
         final int SEMIGLOSS_PER_WALL = semiPerWall;
         final int PRIMER_PER_WALL = primerPerWall;
 
-        //JProgressMonitor object being initialized
-        //I did not make a JOptionPane object as most of its methods are static and no object is required
+
 
         //parsing the strings from the input windows into shorts
         short roomLength = (short) checkJOptionInput("Please enter the length of your room in CM",0,0);
@@ -192,19 +204,18 @@ public class PaintCalculatorProgram2 implements ActionListener {
         short roomHeight = (short) checkJOptionInput("Please enter the height of your room in CM",0,0);
 
         //using the getWallArea function to return the surface area of the relevant walls. answer is returned in square centimeters
-        //the reason this has to be a double is that answerM below it can have decimals, and in division, at least one variable needs to be a double to get a double as the answer
         double answerCM = getWallArea(roomLength, roomWidth, roomHeight);
 
         //converting square cm to square meters using a constant conversion factor
         double answerM = answerCM/SQUARE_CM_IN_SQUARE_M;
 
-        //rounding to 2 decimal places
-        answerM = roundDouble(answerM,sigDigs);
+        //rounding to 3 significant digits
+        answerM = roundDouble(answerM, sigDigs);
 
-        //using another conversion factor to find litres of paint required. some work went into rounding this value. I found a class called BigDecimal. the constructor takes in a double, and it has several methods and rounding modes
+        //using another conversion factor to find litres of paint required.
         double paintLitresRequired = answerM / METERS_PER_PAINT;
 
-        //rounding to 2 decimal places
+        //rounding to 3 sig digs
         double dPaintLitresRequired = roundDouble(paintLitresRequired,sigDigs);
 
         //More conversion factors to get desired values.
@@ -216,23 +227,25 @@ public class PaintCalculatorProgram2 implements ActionListener {
         int semiCansRequiredInt = (int) Math.ceil(semiCansRequiredDecimal);
         int primerCansRequiredInt  = (int) Math.ceil(primerCansRequiredDecimal);
         double surfaceAreaInUnit = answerM;
+        //setting up conversion into the users desired unit. Notice that "unit" is an integer. I tried to use enumeration instead but passing the index of what was selected in the menu was more convenient for me
+        //using conversion factors to calculate values in new units. Also setting the unit symbol
         String unitSymbol = "M";
         switch(unit){
             case 0:
                 surfaceAreaInUnit = answerM;
-                unitSymbol = "M";
+                unitSymbol = "m";
                 break;
             case 1:
                 surfaceAreaInUnit = answerCM;
-                unitSymbol = "CM";
+                unitSymbol = "cm";
                 break;
             case 2:
                 surfaceAreaInUnit = answerM*SQUARE_FEET_IN_SQUARE_M;
-                unitSymbol = "FT";
+                unitSymbol = "ft";
                 break;
             case 3:
                 surfaceAreaInUnit = answerM*SQUARE_INCH_IN_SQUARE_M;
-                unitSymbol = "IN";
+                unitSymbol = "in";
                 break;
         }
         //breaking up the output into different strings representing a different part of the message
@@ -243,75 +256,70 @@ public class PaintCalculatorProgram2 implements ActionListener {
 
 
         String sCans = "Cans of primed required: "+primerCansRequiredInt+"\nCans of semi-gloss required: "+semiCansRequiredInt;
+
+        //using the rounded (floored) value if the rounding mode is set to integer
+        int roundedSurfaceArea = (int)Math.floor(surfaceAreaInUnit);
         if(roundingMode == 0){
-            int roundedSurfaceArea = (int)Math.floor(surfaceAreaInUnit);
             sSurfaceArea = "Total surface area to be painted: "+ roundedSurfaceArea+""+unitSymbol+"\u00B2\n";
         }
+
+        //Using this to prevent the showing of an extra decimal if the value happens to be a whole number
         if(dPaintLitresRequired == Math.floor(dPaintLitresRequired)){
             int intPaintLitresRequired = (int)Math.floor(dPaintLitresRequired);
             sLitres = "Litres of paint required per coat: "+intPaintLitresRequired+"L\n";
 
         }
 
+        //Using this to prevent the showing of an extra decimal if the value happens to be a whole number
+        if(surfaceAreaInUnit == Math.floor(surfaceAreaInUnit)){
+            sSurfaceArea = "Total surface area to be painted: "+ roundedSurfaceArea+""+unitSymbol+"\u00B2\n";
 
-        //using a for loop going from 0 to 100 to set the progress on my progress bar. using a delay of 100ms for each percentage completion.
-        /*for(int i =0; i<=100; i++){
-            ProgressMonitor progressBar = new ProgressMonitor(null, "Calculating paint needed", "note", 0,100);
+        }
+        //No progress bar included in this project due to my goal of maximizing user interface and user experience
 
-            long startTime = System.currentTimeMillis();
-            long elapsedTime = 0L;
-            while(elapsedTime<50){
-                elapsedTime = (new Date()).getTime()-startTime;
-            }
-            progressBar.setProgress(i);
-
-            //setting a different note based on what percentage of progress has been completed
-            if(i<=30){
-                progressBar.setNote("Calculating surface area...");
-            }
-            else if(i<=60){
-                progressBar.setNote("Calculating litres per coat..");
-            }
-            else{
-                progressBar.setNote("Calculating cans required...");
-            }
-        }*/
-
-        //showing a message containing all the strings after the progress bar has completed
+        //Show a message dialog with all the strings displaying
         JOptionPane.showMessageDialog(null, sIntro+sSurfaceArea+sLitres+sCans, "Calculations", JOptionPane.INFORMATION_MESSAGE);
 
     }
 
+    //actionPerformed method that gets called everytime a certain action regarding a certain object is performed
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
+
+        //if the action was the pressing of the submit button
         if(actionEvent.getSource() == submitButton){
             double paintVolume;
+            //if the user didn't select a custom volume use the default one
             if(paintVolumeSelector.getSelectedItem()== paintVolumeOptions[0]){
                 paintVolume = defaultPaintVolume;
             }
+            //if the user selected a custom volume use that
             else{
                 paintVolume = Double.parseDouble((String) paintVolumeSelector.getSelectedItem());
 
             }
-            System.out.println("paint can volume is "+paintVolume);
+            //open the paint program with all the selected values from all the dropdown menus
             openPaintProgram(unitMenu.getSelectedIndex(), paintVolume, (int)primerSelector.getSelectedItem(),  (int)semiSelector.getSelectedItem(), (int)precisionSelector.getSelectedItem(), roundingModeSelector.getSelectedIndex());
 
         }
-        if(actionEvent.getSource() == (paintVolumeSelector) && paintVolumeSelector.getSelectedItem() == "Custom"){
+
+        //if the action that occurred was the changing of a value in the paint volume dropdown menu, and it was changed to custom
+        if(actionEvent.getSource() == paintVolumeSelector && paintVolumeSelector.getSelectedItem() == "Custom"){
+
+            //if the user wanted a custom value make an input box to get that value from them, and if they close the window, go back to the default
             double newVol = checkJOptionInput("Please select new paint volume per can",1,defaultPaintVolume);
+
+            //if they selected the same value as the default, don't make a new entry just select default
             if(newVol == defaultPaintVolume){
                 paintVolumeSelector.setSelectedItem(paintVolumeOptions[0]);
             }
+            //otherwise, add the new value they chose to the dropdown menu and have it be selected
             else {
                 paintVolumeSelector.addItem(String.valueOf(newVol));
                 paintVolumeSelector.setSelectedItem(String.valueOf(newVol));
             }
         }
-        if(actionEvent.getSource() == unitMenu){
-            System.out.println("something happened in the menu");
-            String selected = (String) unitMenu.getSelectedItem();
-            System.out.println(selected);
-        }
+
     }
 
 }
